da3a2ec14630eaf2660da061a7a6906decb6b5f7
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-06-06 01:39:23 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // ⚠️ Replace with your deployed Google Apps Script Web App URL
-  API_URL: 'https://AKfycbx_B4t_J9IPHMoVzPBHxAEK9pUo3fEGK9Us14kHuNMI-OsMZR-lblNCFybCj9gJ3A5H/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwV-ocQv0zw3MruJfLaFT7SYnOBgJBaSdxBM_Oo0CLFUw4CDvQUGywd9h8rFyYZsDOT/exec',
 
   COURSES: [
     'Geometric Design of Road and Streets (CEng 3201)',
